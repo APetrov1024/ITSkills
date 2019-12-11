a9f94bbb3e1f2547fa3a868ee6109fe70a048806
admin <admin@DESKTOP-V2PRUR7>
2019-12-11 11:48:47 +0300

    validation in AddEmployee added

--- ITSkills/AddEmployeeForm.cs
+++ ITSkills/AddEmployeeForm.cs
@@ -28,23 +28,38 @@ namespace ITSkills
                 professionComboBox.Items.Add(profession);
             };
         }
-
+        private bool Validation()
+        {
+            string fieldName = "";
+            if (nameTextBox.Text == "") fieldName = "имя";
+            if (lastNameTextBox.Text == "") fieldName = "фамилия";
+            if (professionComboBox.SelectedValue is null) fieldName = "профессия";
+            if (fieldName != "")
+            {
+                MessageBox.Show("Поле " + fieldName + " не может быть пустым", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
         private void addButton_Click(object sender, EventArgs e)
         {
-            var dataContext = new ITSkillsDataContext();
-            Employees employee = new Employees();
-            employee.Name = nameTextBox.Text;
-            employee.LastName = lastNameTextBox.Text;
-            if (patronymicTextBox.Text == "")
-                employee.Patronymic = null;
-            else
-                employee.Patronymic = patronymicTextBox.Text;
-            employee.DateOfBirth = birthDateTimePicker.Value;
-            string newProfession = professionComboBox.SelectedItem.ToString();
-            employee.ProfessionID = dataContext.Professions.SingleOrDefault(p => p.Profession == newProfession).Id;
-            dataContext.Employees.InsertOnSubmit(employee);
-            dataContext.SubmitChanges();
-            this.Close();
+            if (Validation())
+            {
+                var dataContext = new ITSkillsDataContext();
+                Employees employee = new Employees();
+                employee.Name = nameTextBox.Text;
+                employee.LastName = lastNameTextBox.Text;
+                if (patronymicTextBox.Text == "")
+                    employee.Patronymic = null;
+                else
+                    employee.Patronymic = patronymicTextBox.Text;
+                employee.DateOfBirth = birthDateTimePicker.Value;
+                string newProfession = professionComboBox.SelectedItem.ToString();
+                employee.ProfessionID = dataContext.Professions.SingleOrDefault(p => p.Profession == newProfession).Id;
+                dataContext.Employees.InsertOnSubmit(employee);
+                dataContext.SubmitChanges();
+                this.Close();
+            }
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
